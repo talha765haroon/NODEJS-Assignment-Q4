9426fa9f7c9f2bce981291806e1f8b6a62a580ba
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:13:01 +0500

    Add files via upload

---
+++ q4.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Ques # 04:
+Famous Quote, find a  quote from a famous person you admire. print the quote and the name of its author your output should something like the following,including the quotation mark:
+
+Albert Einstein once said, "A person who never made a mistake never tried anything new."*/
+let famous_Name = ("Hazrat Ali");
+console.log(`"${famous_Name} Once said, "A person who never made a mistake never tried anything new." `);
---
+++ q4.ts
@@ -0,0 +1,9 @@
+/* Ques # 04:
+Famous Quote, find a  quote from a famous person you admire. print the quote and the name of its author your output should something like the following,including the quotation mark:
+
+Albert Einstein once said, "A person who never made a mistake never tried anything new."*/
+
+
+let famous_Name:string = ("Hazrat Ali");
+
+console.log(`"${famous_Name} Once said, "A person who never made a mistake never tried anything new." `)
